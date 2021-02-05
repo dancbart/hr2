e6fc6a30197c2a6157e69f1a0d6f24fa6df834e4
Dan <daniel@barton-web.com>
2021-02-04 18:03:51 -0800

    v1

--- hr2.py
+++ hr2.py
@@ -1,6 +1,9 @@
 import pandas as pd
-import matplotlib.pyplot as pyplot
+import matplotlib.pyplot as plt
 hr_data = pd.read_csv('/users/danielbarton/github/hr2/debs.dat')
 #print the result
 #hr_data[:10]
-    lines
+    lines = f.readlines()
+    x = [)line.split()[18]] for line in lines]
+    y = [(line.split()[20]) for line in lines]
+plt.plot(x ,y,'o')
