1ba543c09df6ed0beaa78030d3af4e813fd350df
dan <daniel@barton-web.com>
2021-02-08 09:56:51 -0800

    Final edit, including documentation/notes for someone reading the code.  Ready to submit.

--- hr3.py
+++ hr3.py
@@ -1,14 +1,12 @@
-# desired columns: V    log(Teff1)
-
+#Plot astronomical data from http://www.astro.keele.ac.uk/jkt/debcat/
 import pandas as pd
 import plotly.express as px
-import plotly.graph_objects as go
-import numpy as np
+#converted ascii file to csv, then read it here
 hr_data = pd.read_csv('/users/danielbarton/github/hr2/debs3.csv')
+#Plotting Luminosity "log(L1)" vs. Temperature "log(Teff1)"
 fig = px.scatter(hr_data, x= 'log(Teff1)', y= 'log(L1)',
             title = "DEBCat: well-studied eclipsing binaries",
             labels = {'log(Teff1)': "Temperature (K)", 'log(L1)': "Luminosity"},
             template="simple_white",
             )
-
 fig.show()
